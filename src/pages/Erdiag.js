6225170afb169d893eb97bf2f00910cee40d0b15
Mayank Raj <mayankraj7100@gmail.com>
2024-04-19 22:34:27 +0530

    Er modified

--- src/pages/Erdiag.js
+++ src/pages/Erdiag.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import Er from "./Er.png"
+import Er from "./jhy.png"
 const Erdiag = () => {
   return (
     <div style={{paddingTop : "38px" , display : "flex" , justifyContent : "center" , alignItems : "center" }}>
-        <img src = {Er} style={{height : "537px"}}/>
+        <img src = {Er} style={{height : "680px" , width : "1000px"}}/>
     </div>
   )
 }
